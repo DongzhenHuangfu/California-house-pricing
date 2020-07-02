a9541c0e27c746339a9f2c7dd06ca145bdc1355a
DongzhenHuangfu <wyatt_h@outlook.com>
2020-07-02 22:48:11 +0200

    fix: x should be translated tio xi and standarization process should be implemented

--- app/main.py
+++ app/main.py
@@ -5,12 +5,33 @@ import pickle
 
 app = FastAPI()
 
+def trans_xi(data, max_grade=4, show=False):
+    ret = np.ones(shape=(len(data), pow(max_grade+1, len(data))), dtype=np.float64)
+    for i in range(len(data)):
+        for grade in range(max_grade+1):
+            if i == 0:
+                if grade != 0:
+                    ret[:, grade] = data[i] * ret[:, grade-1]
+            else:
+                now_vec = ret[:, 0:pow(max_grade+1, i)]
+                if grade != 0:
+                    for j in range(pow(max_grade+1, i)):
+                        ret[:, grade * pow(max_grade+1, i) + j] = now_vec[:, j] * pow(data[i], grade)
+    return ret
+
+
 with open("./model.pickle", 'rb') as f:
 	data = pickle.load(f)
-w = data[0]
+	w = data[0]
+	mu = data[2]
+	divid = data[3]
 
 @app.get("/house-pricing/")
 async def read_items(features: List[float]):
 	input = np.array(features, dtype=np.float16)
-	price = input.dot(w)[0]
+	
+	X = trans_xi(input)
+	X_stand = ((X - mu) / divid).astype(np.float16)
+
+	price = X_stand.dot(w)[0]
 	return {price}
