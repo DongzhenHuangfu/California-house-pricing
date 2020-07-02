e7366b0432063dd7ad05e9fbf9c3d7265b3b475f
DongzhenHuangfu <wyatt_h@outlook.com>
2020-07-02 22:24:10 +0200

    deat: finish model implementation at /house-pricing, need testing

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.8
 
-RUN pip install fastapi uvicorn numpy
+RUN pip install fastapi uvicorn numpy typing
 
 EXPOSE 5000
 
--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 import numpy as np
+from typing import List
 import pickle
 
 app = FastAPI()
 
-@app.get("/house-pricing/{features}")
-def predict(features: float):
-	return {features}
+with open("./model.pickle", 'rb') as f:
+	data = pickle.load(f)
+w = data[0]
+
+@app.get("/house-pricing/")
+async def read_items(features: List[float]):
+	input = np.array(features, dtype=np.float16)
+	price = input.dot(w)[0]
+	return {price}
